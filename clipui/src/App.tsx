ff10e6c6a1e3869818454555b795fddd11fa74c9
Kevin Clerkin <kevin.clerkin1@gmail.com>
2024-05-24 01:03:52 +0100

    Add handle file change function and input element

--- clipui/src/App.tsx
+++ clipui/src/App.tsx
@@ -1,13 +1,21 @@
-import React, { useState, DragEvent } from 'react';
+import React, { useState, DragEvent, ChangeEvent } from 'react';
 import './App.css';
 
 const App: React.FC = () => {
   const [file, setFile] = useState<File | null>(null);
   const [resultImage, setResultImage] = useState<string | null>(null);
+  
+  
   const handleDrop = (e: DragEvent<HTMLDivElement>) => {
     e.preventDefault();
     const droppedFile = e.dataTransfer.files[0];
     setFile(droppedFile);
+  };
+
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      setFile(e.target.files[0]);
+    }
   };
   
   
@@ -18,6 +26,13 @@ const App: React.FC = () => {
           className="drop-zone" 
           onDrop={handleDrop} 
         >
+          <h1>rmbg</h1>
+          <input 
+            type="file" 
+            onChange={handleFileChange} 
+            style={{ display: 'none' }} 
+            id="fileInput"
+          />
           <button onClick={() => document.getElementById('fileInput')?.click()}>
             Select Image
           </button>
