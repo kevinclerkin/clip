012591df86d760fbceff7d7cfadb830344881e9f
Kevin Clerkin <kevin.clerkin1@gmail.com>
2024-05-26 19:11:19 +0100

    Add component spinner and styling

---
+++ clipui/src/Components/Spinner/Spinner.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Spinner = (props: Props) => {
+   return(
+    <div className="spinner-overlay">
+    <div className="spinner" />
+    </div>
+)
+}
+
+export default Spinner
