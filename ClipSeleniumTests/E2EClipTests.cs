d00159253957dd79e78b86001076722fea2502ec
Kevin Clerkin <kevin.clerkin1@gmail.com>
2024-09-19 22:18:38 +0100

    Add headless mode and refactor with web driver manager

---
+++ ClipSeleniumTests/E2EClipTests.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium.Chrome;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+using WebDriverManager.Helpers;
+
+namespace ClipSeleniumTests
+{
+    public class E2EClipTests
+    {
+        [Fact]
+        public void DisplayCorrectTitle_On_NavigateToHomePage()
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig(), VersionResolveStrategy.MatchingBrowser);
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("--headless");
+            
+            using var driver = new ChromeDriver(chromeOptions);
+
+            driver.Navigate().GoToUrl("https://clip-762c7.web.app/");
+            
+            Assert.Equal("Clip", driver.Title);
+        }
+        
+    }
+}
